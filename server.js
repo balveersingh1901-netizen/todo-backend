e9ec8b2d232f0d112957b59a0734690878eb24d4
Balveer Singh <balveersingh1901@gmail.com>
2025-11-22 03:04:24 +0530

    Fix CORS and MongoDB connection

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
-// Middlewares
-app.use(express.json());
-app.use(cors());
+// 1) Proper CORS FIX
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 
-// Connect to MongoDB
+app.use(express.json());
+
+// 2) CONNECT TO MONGODB
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
@@ -24,17 +31,18 @@ const connectDB = async () => {
   }
 };
 
-module.exports = connectDB;
+// **CALL THE FUNCTION (This was missing!)**
+connectDB();
 
-// Routes
+// 3) ROUTES
 app.use("/tasks", taskRoutes);
 
-// Test route
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("Backend running successfully!");
 });
 
-// Start server
+// 4) START SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
